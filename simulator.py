df53827a3f1377d5b9373fe615a7574fcd9862a9
Tynan <tynandavis@gmail.com>
2024-06-19 19:14:47 -0700

    built out simul more getting ready for strategy

--- simulator.py
+++ simulator.py
@@ -1,7 +1,13 @@
-import yfinance as yfin
+import yfinance as yf
+from datetime import datetime as dt
+import pandas as pd
+from datetime import timedelta
+import numpy as np
+import mplfinance as mpf
+import io
 
 """ But say you have one (or more) arguments in method1:
-
+pip
 def method1(param):
     return 'hello ' + str(param)
 
@@ -16,7 +22,56 @@ method2(lambda: method1('world'))
 method2(lambda: method1('reader'))
 >>> hello reader """
 
-
-
 def simulate_strategy_day(symbol, day, strategy):
+    if dt.strptime(day, '%Y-%m-%d') < (dt.today() - timedelta(days=7)):
+        print ("Error: the day you selected is more than 7 days ago")
+        return 
+    #first lets get the data for the day for the symbol
+    df = yf.download(symbol, start=day, period="1d", interval="1m")
     
+    #now we have the data we want to run our strategy on each 1m interval to see how we did
+    #TODO
+    #iterate on each row of data and call the strategy
+    actionlist = []
+    for row in df.itertuples():
+        print("Data: " + row)
+        action = strategy(row)
+        actionlist.append(action)
+        print("Result: "+ action)
+    
+    #make sure our x axis index is formatted property
+    df.index = pd.to_datetime(df.index)
+    df.index = df.index.tz_localize(None)
+    #data variable and formatting to display data on top off graph
+    #now lets create our buy/sell/other important flags from our strategy data set to add as an overlay on the plot
+    """ data = '''
+    Time Price
+    "2022-06-07 11:20:00" 412.66
+    "2022-06-07 12:30:00" 411.350
+    "2022-06-07 13:50:00" 413.290
+    "2022-06-07 15:00:00" 414.109
+    "2022-06-09 13:25:00" 409.660
+    "2022-06-10 09:50:00" 394.130
+    '''
+    sell_df = pd.read_csv(io.StringIO(data), delim_whitespace=True)
+    sell_df['Time'] = pd.to_datetime(sell_df['Time'])
+    sell_df.set_index('Time', inplace=True)
+    sell_df = sell_df.reindex(df.index, axis='index', fill_value=np.NaN)
+    apdict = mpf.make_addplot(sell_df['Price'], type='scatter', markersize=200, marker='^') """
+    
+    #candlestick ploit the data for the day
+    mpf.plot(df,
+         type="candle", 
+         title = "Stock: "+symbol+" Day: "+day,  
+         style="yahoo", 
+         volume=True, 
+         figratio=(20.00, 6.75),
+         returnfig=True,
+         show_nontrading=False
+         #addplot=apdict
+    )
+    mpf.show()
+    
+    return
+
+simulate_strategy_day("META", dt.today().strftime('%Y-%m-%d'), "s")
